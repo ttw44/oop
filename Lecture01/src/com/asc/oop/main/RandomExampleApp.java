2f3fc2f2ed59767a62af842f07972d44e3fc8bb7
ttw44 <80911367+ttw44@users.noreply.github.com>
2021-03-18 12:32:13 -0400

    Add files via upload

---
+++ Lecture01/src/com/asc/oop/main/RandomExampleApp.java
@@ -0,0 +1,22 @@
+package com.asc.oop.main;
+
+import java.util.Scanner;
+
+public class RandomExampleApp 
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Welcome to the program. \n Enter your name in the following format: [first last]");
+		String input = "";
+		String fname;
+		String lname;
+		
+		Scanner sc = new Scanner(System.in);
+		input = sc.nextLine();
+		fname = input.substring(0, input.indexOf(' ', 0));
+		lname = input.substring(input.indexOf(' ', 0) + 1);
+		
+		System.out.println("Your name is: " + fname + " " + lname);
+		sc.close();
+	}
+}
